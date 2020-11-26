c29314d1996b74cc6fb852407ba7700ffcfc1db6
jeison-araya <jeison.arayamena@ucr.ac.cr>
2020-11-26 03:00:23 -0600

    Line model modified

--- app/telephone-database/src/app/models/line.model.ts
+++ app/telephone-database/src/app/models/line.model.ts
@@ -1,10 +1,10 @@
 
 export class Line {
-    telephoneNumber: string | undefined;
-    customerId: string | undefined;
-    customerFirstName: string | undefined;
-    customerLastName: string | undefined;
-    customerAddress: string | undefined;
-    customerEmail: string | undefined;
-    type: string | undefined;
+    telephoneNumber = '';
+    customerId = '';
+    customerFirstName = '';
+    customerLastName = '';
+    customerAddress = '';
+    customerEmail = '';
+    type = '';
 }
