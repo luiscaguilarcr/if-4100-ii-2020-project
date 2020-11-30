f81034e0b93b8b04e9554e746330c2bc804fa1b5
luiscaguilarcr <luiscarlos.aguilar@ucr.ac.cr>
2020-11-30 12:09:19 -0600

    Update

--- app/telephone-database/src/app/models/line_customer.model.ts
+++ app/telephone-database/src/app/models/line_customer.model.ts
@@ -2,7 +2,7 @@ export class LineCustomer {
     telephoneNumber = '';
     customerId = '';
     customerFirstName = '';
-    customerLastNameName = '';
+    customerLastName = '';
     customerAddress = '';
     customerEmail = '';
 }
--- app/telephone-database/src/app/services/line.service.ts
+++ app/telephone-database/src/app/services/line.service.ts
@@ -12,13 +12,13 @@ export class LineService {
 
   public addLine(line: Line) {
     
-    this.databaseService.getPool().then((pool: any) => {
+    return this.databaseService.getPool().then((pool: any) => {
       const insertStatement = 'INSERT INTO [Line] (Telephone_Number, Points_Quantity, Type) values (?,?,?);'
 
       return pool.request()
-        .input('Telephone_Number', this.variableTypes.Int(), line.telephoneNumber)
-        .input('Points_Quantity', this.variableTypes.Int(), line.pointsQuantity)
-        .input('Type', this.variableTypes.tinyint(), line.type)
+        .input('Telephone_Number', this.variableTypes.Int, line.telephoneNumber)
+        .input('Points_Quantity', this.variableTypes.Int, line.pointsQuantity)
+        .input('Type', this.variableTypes.tinyint, line.type)
         .query(insertStatement)
     })
     .catch ((error: any) => {
