f659b24e58233d069144aadea9385ed1a54c70c1
jeison-araya <jeison.arayamena@ucr.ac.cr>
2020-11-30 17:12:43 -0600

    LineBusinessService and Servlet added, Old code deleted

---
+++ back-end/ServerManagerAPI/src/main/java/edu/ucr/recinto_paraiso/bases_datos/proyecto/telephone_api/domain/LineCustomer.java
@@ -0,0 +1,2 @@
+package edu.ucr.recinto_paraiso.bases_datos.proyecto.telephone_api.domain;public class LineCustomer {
+}
---
+++ back-end/ServerManagerAPI/src/main/java/edu/ucr/recinto_paraiso/bases_datos/proyecto/telephone_api/logic/bussiness/LineBusinessService.java
@@ -0,0 +1,2 @@
+package edu.ucr.recinto_paraiso.bases_datos.proyecto.telephone_api.logic.bussiness;public class LineBusinessService {
+}
