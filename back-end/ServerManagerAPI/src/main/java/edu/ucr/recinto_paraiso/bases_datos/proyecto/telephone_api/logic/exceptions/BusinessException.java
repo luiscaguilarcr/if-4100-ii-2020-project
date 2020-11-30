a3a821419f2257622cdfaed5302c10f2b5aedc60
jeison-araya <jeison.arayamena@ucr.ac.cr>
2020-11-30 17:58:44 -0600

    Unused exception codes deleted

--- back-end/ServerManagerAPI/src/main/java/edu/ucr/recinto_paraiso/bases_datos/proyecto/telephone_api/logic/exceptions/BusinessException.java
+++ back-end/ServerManagerAPI/src/main/java/edu/ucr/recinto_paraiso/bases_datos/proyecto/telephone_api/logic/exceptions/BusinessException.java
@@ -8,33 +8,15 @@ package edu.ucr.recinto_paraiso.bases_datos.proyecto.telephone_api.logic.excepti
  * Use null as default or unknown error.
  */
 public class BusinessException extends Exception {
-    /* User exception codes */
-    public static final int USER_OBJECT_NOT_PROVIDED = 51;
-    public static final int USER_EMAIL_NOT_PROVIDED = 54;
-    public static final int USER_EMAIL_NOT_VALID = 54;
-    public static final int USER_NEW_EMAIL_NOT_PROVIDED = 54;
-    public static final int USER_NEW_EMAIL_NOT_VALID = 54;
-    public static final int USER_EMAIL_IN_USE = 54;
-    public static final int USER_CALL_IN_USE = 54;
-    public static final int USER_FIRST_NAME_NOT_PROVIDED = 54;
-    public static final int USER_LAST_NAME_NOT_PROVIDED = 54;;
-    public static final int USER_NEW_FIRST_NAME_NOT_PROVIDED = 54;
-    public static final int USER_NEW_LAST_NAME_NOT_PROVIDED = 54;
-    public static final int USER_PASSWORD_NOT_PROVIDED = 55;
-    public static final int USER_PASSWORD_NOT_SECURE = 55;
-    public static final int USER_NEW_PASSWORD_NOT_PROVIDED = 55;
-    public static final int USER_NEW_PASSWORD_NOT_SECURE = 55;
-
     /* Call exception codes */
     public static final int CALL_OBJECT_NOT_PROVIDED = 1;
     public static final int CALL_TELEPHONE_NUMBER_NOT_PROVIDED = 1;
     public static final int CALL_DESTINATION_TELEPHONE_NUMBER_NOT_PROVIDED = 1;
     public static final int CALL_END_DATE_NOT_PROVIDED = 1;
     public static final int CALL_START_DATE_NOT_PROVIDED = 1;
-
     /* CallServlet exception codes */
     public static final int CALL_NOT_DELETED = 1;
-
+    /* Json format */
     public static final int JSON_FORMAT_EXCEPTION = 87;
     /* Line */
     public static final int LINE_TELEPHONE_NUMBER_IN_USE = 54;
