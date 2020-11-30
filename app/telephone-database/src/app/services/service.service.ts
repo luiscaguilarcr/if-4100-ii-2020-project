c2d9d0cb5b6e74458dc42b98b29291e630b7e73f
luiscaguilarcr <luiscarlos.aguilar@ucr.ac.cr>
2020-11-29 19:03:47 -0600

    update

--- app/telephone-database/src/app/services/service.service.ts
+++ app/telephone-database/src/app/services/service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { DatabaseService } from './database.service';
+import { service } from '../models/service.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  variableTypes : any;
 
-  constructor() { }
+  constructor(private databaseService: DatabaseService) {this.variableTypes = databaseService.util()}
+
+  public addCall(service: Service) {
+    
+    this.databaseService.getPool().then((pool: any) => {
+      const insertStatement = 'INSERT INTO [Line] (Service_Code, Name, Description, Cost) values (?,?,?,?);'
+
+      return pool.request()
+        .input('Service_Code', this.variableTypes.Int(), service.serviceCode)
+        .input('Name', this.variableTypes.Int(), service.name)
+        .input('Description', this.variableTypes.Int(), service.description)
+        .input('Cost', this.variableTypes.DateTime(), service.cost)
+ 
+        .query(insertStatement)
+    })
+    .catch ((error: any) => {
+        console.log(error);
+    });
+    
+  }
 }
